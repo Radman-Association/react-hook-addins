c0455379646f519a8063621dfd8bddd2094c34ba
meisammofidi <meisam.mofidi@yahoo.com>
2022-06-05 22:26:00 -0400

    Initial app by converting to react hook

---
+++ src/taskpane/components/App.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { DefaultButton } from "@fluentui/react";
+import Header from "./Header";
+import HeroList from "./HeroList";
+import Progress from "./Progress";
+
+/* global Word, require */
+const init = {
+  listItems: [],
+};
+
+const App = (props) => {
+  const [items, setItems] = useState(init);
+
+  useEffect(() => {
+    setItems({
+      listItems: [
+        {
+          icon: "Ribbon",
+          primaryText: "Achieve more with Office integration",
+        },
+        {
+          icon: "Unlock",
+          primaryText: "Unlock features and functionality",
+        },
+        {
+          icon: "Design",
+          primaryText: "Create and visualize like a pro",
+        },
+      ],
+    });
+  }, []);
+
+  const click = async () => {
+    return Word.run(async (context) => {
+      /**
+       * Insert your Word code here
+       */
+
+      // insert a paragraph at the end of the document.
+      const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
+
+      // change the paragraph color to blue.
+      paragraph.font.color = "blue";
+
+      await context.sync();
+    });
+  };
+
+  // render() {
+  const { title, isOfficeInitialized } = props;
+
+  if (!isOfficeInitialized) {
+    return (
+      <Progress
+        title={title}
+        logo={require("./../../../assets/logo-filled.png")}
+        message="Please sideload your addin to see app body."
+      />
+    );
+  }
+
+  return (
+    <div className="ms-welcome">
+      <Header logo={require("./../../../assets/logo-filled.png")} title={title} message="Welcome" />
+      <HeroList message="Discover what Office Add-ins can do for you today!" items={items.listItems}>
+        <p className="ms-font-l">
+          Modify the source files, then click <b>Run</b>.
+        </p>
+        <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={click}>
+          Run
+        </DefaultButton>
+      </HeroList>
+    </div>
+  );
+};
+
+App.propTypes = {
+  title: PropTypes.string,
+  isOfficeInitialized: PropTypes.bool,
+};
+
+export default App;
